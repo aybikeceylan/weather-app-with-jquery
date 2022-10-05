a3104f86aaa7fab1750832552f4351af444a4c7a
aybike <aybikesayar@gmail.com>
2022-10-05 15:46:50 +0300

    continue

---
+++ weather.js
@@ -0,0 +1,8 @@
+const formJS = document.querySelector("form")[0]
+// JQUERY === $
+// const formjquery = jQuery("form").eq(0)
+const formJquery = $("form").eq(0)
+console.log(formJS)
+console.log(formJquery);
+
+//get (index), eq(index)
